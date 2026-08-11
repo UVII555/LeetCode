7ffefb660449d09d89597d9c72ab331040b50b32
UTSAV SINGH <singhutsav555@gmail.com>
2026-08-11 22:25:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (55.4%) - LeetHub

--- 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -7,7 +7,11 @@ class Solution {
 
 
 // ~ ye bhi use krt skte thae lekin ye 32 bit ko convert krta hai like 5 is 101 but in 32 bit it will be 0000000000000---101 fir isko ~ convert krne pr 111111111-----010 ata 
-
+// | ye bit wise OR hai ... ek True hoga tb ansewer true hoga only 0 | 0 ==0 
+// step 1 of mask<<1) | 1
+// s1 -- 1<<1 = 10<<1 = 110 <<1 = 111
+//s2 011<<1 = 111
+// so 111 | 001 = 111
 // so left shift operator use kr k and 1 kiya gya hai like mask =1 se starat hua hai 1<<1
 // == 10<<1 == 110 <<1 == 111 hoga fir ^ XOR use kr k flip ke dete hai 101 ( 5 ko) fir ayega 111 ^ 101 = 010 output =5
         }
